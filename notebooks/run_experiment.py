92374ae35c8b399dfbe7870b6a3fef2c4c58b469
Mattias Appelgren <mattiasappel@gmail.com>
2019-04-24 13:11:12 +0100

    update of notebooks

---
+++ notebooks/run_experiment.py
@@ -0,0 +1,6 @@
+from correctingagent.experiments.experiment import run_experiment, add_big_experiment
+
+#add_big_experiment('bigthreerules_old_teacher_no_corr', 'fixed_bw')
+add_big_experiment('bigthreerules_new_teacher_no_corr', 'fixed_bw')
+
+
